8393f3f93c816b622a64e6d763bdfa34c63a2d60
john-paul360 <360stpaulj@gmail.com>
2025-05-14 09:58:54 +0100

    added some content

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -20,13 +20,12 @@ const Nav = ({ bg }) => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                {" "}
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
+                />
               </svg>
             </div>
             {isLoggedIn ? (
